71a28852ae7f1a7ad63d9fca74e61f34a0ac8a01
Paul-Emile MINY <paulemile.miny@gmail.com>
2014-03-17 10:50:54 +0100

    move server code to the root directory

--- client/app/config/config.js
+++
@@ -1,5 +0,0 @@
-angular.module('polldayApp').constant('SOCKET', {
-    'PORT': '9091',
-    'HOST': '127.0.0.1'
-});
-
--- client/config/deploy/dev.rb
+++
@@ -1,6 +0,0 @@
-role :web, "#{application}.dev.scoua.de"
-
-set :deploy_to,     "/var/www/#{application}.dev.scoua.de"
-set :domain,      "#{application}.dev.scoua.de"
-
-ssh_options[:user] = 'capistrano'
